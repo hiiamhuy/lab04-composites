eca1121c3a7685e0f98f3e4cf430a33b9b208bc6
hiiamhuy <hiiamhuy@uw.edu>
2017-01-24 11:06:26 -0800

    update

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,24 @@ interface IDomElement {
   print();
 }
 
+class TextNode implements IDomElement {
+  private text: string
+  print() {
+    console.log(this.text)
+  }
+}
+
+class DomElement implements IDomElement {
+  private element: string
+  private elements = [] 
+
+  print() {
+    for (let element of this.elements) {
+      element.print();
+    }
+  }
+
+  add(element: IDomElement) {
+    this.elements.push(element);
+  }
+}
